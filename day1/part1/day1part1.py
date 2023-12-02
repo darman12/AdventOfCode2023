2b93b6c966626816346ae8ea04ee6b09d5aed12d
Spencer Freebairn <spencer.freebairn@outlook.com>
2023-12-02 11:54:51 -0600

    add logic to replace written numbers with digit
    add logic to identify digits in string

--- day1/part1/day1part1.py
+++ day1/part1/day1part1.py
@@ -1,12 +1,14 @@
-with open("input.txt") as file: 
+with open("input.txt") as file:
     input = file.read().replace('\n',',').split(",")
 
 # print(puzzleInput.read())
 
 # parsedInput = input.split(",")
-    
+
+
+
 numbers = [
-     "zero"    
+     "zero"
     ,"one"
     ,"two"
     ,"three"
@@ -18,13 +20,42 @@ numbers = [
     ,"nine"
 ]
 
-# for each in input:
-#     if "four" in each.lower():
-#         print(each)
+def replaceNumber(i, j):
+    if i == "zero":
+        return j.replace(i, "0")
+    elif i == "one":
+        return j.replace(i, "1")
+    elif i == "two":
+        return j.replace(i, "2")
+    elif i == "three":
+        return j.replace(i, "3")
+    elif i == "four":
+        return j.replace(i, "4")
+    elif i == "five":
+        return j.replace(i, "5")
+    elif i == "six":
+        return j.replace(i, "6")
+    elif i == "seven":
+        return j.replace(i, "7")
+    elif i == "eight":
+        return j.replace(i, "8")
+    elif i == "nine":
+        return j.replace(i, "9")
 
 for i in numbers:
-    print("searching for " + i)
-    for j in input:
-        if i not in j:
-            print("did not find " + i + " in " + j)
-            # print(i)
+    for j in range(len(input)):
+        if i in input[j]:
+            input[j] = replaceNumber(i, input[j])
+        # else:
+        #     print("did not find " + i + " in " + j)
+        #     # print(i)
+
+for j in input:
+    print(j)
+
+for each in input:
+    for each in each:
+        if each.isdigit():
+            print(each + " is a digit")
+        else:
+            print(each + " is not a digit")
