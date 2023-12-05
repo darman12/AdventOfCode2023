f0d90575182cefd278e845525122e1615ca3d2a6
Spencer Freebairn <spencer.freebairn@outlook.com>
2023-12-05 00:06:18 -0600

    initial commit

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,18 @@
 with open("input.txt") as file:
     input = file.read().split("\n")
 
+gameData = [
+    {
+        "ID": 0,
+        "diceSets": {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        },
+        "power": 0
+    }
+]    
+
 colorMaximums = {
     "red": 12,
     "green": 13,
@@ -13,8 +25,32 @@ def validDraw(colorGroup):
     
     if numDice > colorMaximums[color]:
         return False
+    
+def getPower(cubeSet):
+    print("::getPower cubeSet")
+    
+    colorMinimums = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+
+    for color in cubeSet:
+        if each > colorMinimums[each]:
+            print(str(each) + " is greater than " + colorMinimums[each])
+            colorMinimums[each] = each
+    
+    power = 1
+
+    for each in colorMinimums:
+        power *= each
+
+    print("power: " + str(power))
 
 sum = 0
+power = 0
+
+
 
 for game in input:
 
@@ -22,6 +58,16 @@ for game in input:
     game = game.replace("Game ", "")
     game = game.split(":")
     game[1] = game[1].strip()
+
+    gameData[game].set("ID", int(game[0]))
+
+    for cubeSet in game[1].split(";"):
+        cubeSet = cubeSet.strip()
+        colorGroups = cubeSet.split(",")
+
+        for colorGroup in colorGroups:
+            gameData[game].set()
+
     
     validGame = True
 
@@ -34,7 +80,11 @@ for game in input:
             if validDraw(colorGroup) == False:
                 validGame = False
 
+        power += getPower(cubeSet)
+
     if validGame == True:
         sum += int(game[0])
 
-print("The answer is " + str(sum))
+
+print("Part 1 answer: " + str(sum))
+print("Part 2 answer: " + str(power))
